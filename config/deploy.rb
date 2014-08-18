08ddaace9e42b980cd3272d4e3cbbaedff89b4b4
Hadi Farnoud <hadi.farnoud@gmail.com>
2014-08-19 02:10:38 +0430

    add dotevn-deployment gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,32 +11,9 @@ set :repo_url, 'git@github.com:hadifarnoud/persianpreneur.git'
 set :deploy_to, '/home/deploy/persianpreneur'
 
 set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
