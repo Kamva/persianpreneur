0ce5ed2e0d88ceba6423926c76cd3847a12bff3c
hmn-hf <homon.hf@gmail.com>
2014-08-19 15:39:50 +0430

    Add dependent destroy to ImageUploader

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,9 @@
 class Person < ActiveRecord::Base
 
-	mount_uploader :profile_picture, ImageUploader
-
+	mount_uploader :profile_picture, ImageUploader, dependent: :destroy
+	
 	validates_presence_of :profile_picture
-	validates :full_name, :presence => true
+	validates :full_name, :presence => true, :uniqueness => true
 	# validates :birth_date, :presence => true
 	# validates :location, :presence => true
 
