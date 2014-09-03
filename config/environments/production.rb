e93112b7a03e1c07c8307414bf454ba16b6af3b8
Hadi Farnoud <hadi.farnoud@gmail.com>
2014-09-03 16:06:47 +0430

    About page and Action Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               Rails.application.secrets.EMAIL_DOMAIN,
-  user_name:            Rails.application.secrets.EMAIL_USERNAME,
-  password:             Rails.application.secrets.EMAIL_PASSWORD,
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@persianpreneur.com'}
 end
