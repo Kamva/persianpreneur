5d9e89ba6f3dcc1f057195ac523eaf258f179eef
Hadi Farnoud <hadi.farnoud@gmail.com>
2014-09-03 23:38:48 +0430

    remove 'from' option from action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@persianpreneur.com'}
+  # config.action_mailer.default_options = {from: 'no-reply@persianpreneur.com'}
 end
