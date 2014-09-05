94ddc9c905195c4c4aa9431a47369bf1a799cd6c
hmn-hf <homon.hf@gmail.com>
2014-09-05 16:50:59 +0430

    Add lazy load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,4 @@
 
 $(function(){ $(document).foundation(); });
 $(function(){ $('#person-birthday').fdatepicker({format:'yyyy-mm-dd'}); });
+$(function(){ $("img.lazy").show().lazyload({threshold : 200});} );
