5561df22e76c4ff633279fbabaa3286efa38da4d
Hadi Farnoud <hadi.farnoud@gmail.com>
2015-08-06 12:28:28 +0430

    change server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,12 @@
-set :stage, :production
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-# role :app, %w{deploy@178.162.207.220}
-# role :web, %w{deploy@178.162.207.220}
-# role :db,  %w{deploy@178.162.207.220}
+role :app, %w{deploy@example.com}
+role :web, %w{deploy@example.com}
+role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +14,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '178.162.207.220', user: 'deploy', roles: %w{web app}
+server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
