0f67a1c146618cb0c8f5a6e2106af1c12e4710f9
Hadi Farnoud <hadi.farnoud@gmail.com>
2015-08-11 23:49:46 +0430

    debug level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ config.assets.precompile += %w( vendor/modernizr.js )
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
