44602b38885d30d8077a67db12668db5753b7bdd
Hadi Farnoud <hadi.farnoud@gmail.com>
2016-12-04 13:41:17 +0330

    no message

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  include CarrierWave::rmagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
